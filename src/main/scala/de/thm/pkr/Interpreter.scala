4b8256efc235fbb45ad92b6f9287c94e055b6b16
Jan Müller <jan.m.mueller@mni.thm.de>
2013-01-20 15:12:39 +0100

    Reduced some expressions to simpler or functional expressions.

--- src/main/scala/de/thm/pkr/Interpreter.scala
+++ src/main/scala/de/thm/pkr/Interpreter.scala
@@ -1,207 +1,167 @@
 package de.thm.pkr
 
 import scala.collection.mutable.ArrayBuffer
+import scala.annotation.tailrec
+
+object Types {
+  type OptVal = Option[Value]
+  type Address = Int
+  type ValueOrAddress = Either[Value, Address]
+}
 
 object Storage {
-		type Address = Int
+  import Types._
 
-		// storage is a heap (open ended list of cells)
-		private val heap : ArrayBuffer[Value] = ArrayBuffer();
+  // storage is a heap (open ended list of cells)
+  private val heap = ArrayBuffer[Value]();
 
-		// get content of a cell
-		def apply(a: Address): Option[Value] = 
-			if (a < heap.length) Some(heap(a))
-			else None
+  // get content of a cell
+  def apply(a: Address) =
+    if (a < heap.length) Some(heap(a))
+    else None
 
-		// set value at an allocated address
-		def assign(a: Address, v: Value) {
-			if (a < heap.length) {
-				heap(a) = v
-			} // else ignore
-		}
+  // set value at an allocated address
+  def assign(a: Address, v: Value) =
+    if (a < heap.length) heap(a) = v // else ignore
 
-		// allocate new cell
-		def allocateCell(initVal: Value): Address = {
-			heap += initVal
-			heap.length -1
-		} 
+  // allocate new cell
+  def allocateCell(initVal: Value) = {
+    heap += initVal
+    heap.length - 1
+  }
 }
 
 object Interpreter {
-	import Storage.Address
-	
-	type ValueOrAddress = Either[Value, Address]
-	
-	// Environment
-	// maps names to addresses or values
-	case class Env(env: Map[String, ValueOrAddress] = Map()) {
-  
-		// lookup name
-		def apply(x:String): Option[ValueOrAddress] = env.get(x) 
+  import Types._
 
-		private def +(name: String, definition: ValueOrAddress): Env =
-			Env(env + Pair(name, definition))
-		
-		// add definition of a definable thing 
-		def +(name: String, meaning: Value): Env =
-			Env(env + Pair(name, Left(meaning)))
-		
-		// add definition of an address
-		def +(name: String, meaning: Address): Env =
-			Env(env + Pair(name, Right(meaning)))
-		
-	}
+  // Environment
+  // maps names to addresses or values
+  case class Env(env: Map[String, ValueOrAddress] = Map()) {
 
-	case class inEnv(env: Env) {
-		def eval(exp: Exp) : Option[Value] = {
-			exp match {
-			case IntConst(v) 	=> Some(IntValue(v))
-			case BoolConst(v) 	=> Some(BoolValue(v))
-			case Var(name)	   	=> {
-				val defined = env(name).get
-				defined match {
-				  case Right(a) => Storage(a) // VAR
-				  case Left(v)	=> Some(v)    // CONST
-				} 
-			}    
-			case Greater(e1, e2) 
-				=> 	for (v1 <- eval(e1); 
-						v2 <- eval(e2);
-						v3 <- v1>v2 )
-					yield v3
+    // lookup name
+    def apply(x: String): Option[ValueOrAddress] = env.get(x)
 
-			case Less(e1, e2) 
-				=> 	for (v1 <- eval(e1); 
-						v2 <- eval(e2);
-						v3 <- v1<v2 )
-					yield v3
+    private def +(name: String, definition: ValueOrAddress) =
+      Env(env + Pair(name, definition))
 
-			case Equal(e1, e2) 
-				=> 	for (v1 <- eval(e1); 
-						v2 <- eval(e2);
-						v3 <- v1==v2 )
-					yield v3    	    
-    
-			case Plus(e1, e2) 
-				=> 	for (v1 <- eval(e1); 
-						v2 <- eval(e2);
-						v3 <- v1+v2 )
-					yield v3
+    // add definition of a definable thing 
+    def +(name: String, meaning: Value) = Env(env + Pair(name, Left(meaning)))
 
-			case Minus(e1, e2) 
-				=> 	for (v1 <- eval(e1); 
-						v2 <- eval(e2);
-						v3 <- v1-v2)
-					yield v3
-    	    
-			case Times(e1, e2) 
-				=> 	for (v1 <- eval(e1); 
-						v2 <- eval(e2);
-						v3 <- v1*v2)
-					yield v3
-    	    
-			case Div(e1, e2) 
-				=> 	for (v1 <- eval(e1); 
-						v2 <- eval(e2);
-						v3 <- v1/v2)
-					yield v3
-			}//match
-		}// eval
-		
-		def exec(cmd: Cmd) {
-			cmd match {
-			case If(e1, cThen, cElse) 
-				=> {val v1 = eval(e1)
-					if (v1.isDefined && v1.get.isInstanceOf[BoolValue]) {
-						if (v1.get.asInstanceOf[BoolValue].b) exec(cThen) else exec(cElse)
-					}// else ERROR
-				}//If
-				
-			case Assign(Var(x), e) 
-			  	=> {val r = eval(e)
-			  	  	val l = env(x)
-			  	  	if (l.isDefined) { 
-			  	  	  l.get match {
-			  	  	    case Right(a) => Storage.assign(a, r.get) //x denotes an address
-			  	  	    case Left(a) => None // x denotes non-assignable value
-			  	  	  }
-			  	  	}
-			  	}//Assign
-    	 	
-		    case While(e:Exp, body:Cmd) 
-		    	=> 
-		    	  while(true) {
-		    		val c = eval(e)
-		    		if (c.isDefined && 
-		    		    c.get.isInstanceOf[BoolValue] && 
-		    		    c.get.asInstanceOf[BoolValue].b) {
-		    				exec(body) 
-		    		} else {
-		    			return
-		    		}
-		    	}// While
-    	
+    // add definition of an address
+    def +(name: String, meaning: Address) = Env(env + Pair(name, Right(meaning)))
 
-		    case Print(e) 
-		    	=> {val v = eval(e)
-		    		if (v.isDefined) {
-		    		  println(">> "+eval(e).get)
-		    		} else  {
-		    		  println(">> " + e + " ist undefiniert")
-		    		}
-		    	  } 
-    	
-		    case Block(defs, cmds)
-		    	=> {val nEnv = define(defs)
-		    		cmds.foreach{ inEnv(nEnv).exec(_) }
-		    	}
-		    	
-		    case Call(p, a)
-		    	=> {val arg = eval(a)
-			  	  	val proc = env(p)
-			  	  	if (proc.isDefined && arg.isDefined) { 
-			  	  	  proc.get match {
-			  	  	    case Right(adr) => None //x denotes an address Stored procedures may not be called
-			  	  	    case Left(v) =>  // x denotes non-assignable value
-			  	  	    		v match {
-			  	  	    		  case procedure: ProcValue => procedure.appy(arg.get, env)
-			  	  	    		  case _ => println("ERROR: no procedure");
-			  	  	    		}
-			  	  	  }
-			  	  	}
-		    		
-		    	}
-				
-			}// match
-		} // exec
-		
-	  def define(ds: List[Def]) : Env = {
-	    var nEnv: Env = env
-	    ds foreach { (d) =>
-	      d match {
-	      	case VarDef(x, initVal)
-	      		=> {val v = eval(initVal)
-	      			val adr = Storage.allocateCell(v.get)
-	      			nEnv = nEnv+(x, adr)
-	    	}
-	      	case ConstDef(c, initVal)
-	      		=> {val v = eval(initVal)
-	      		  	nEnv = nEnv+(c, v.get)
-	      	}
-	      	case ProcDef(p, fp, body)
-	      		=> {nEnv = nEnv+(p, ProcValue(fp, body, nEnv))
-	      	}
-	      }
-	    }
-	    nEnv
-	  }// define
-		
-	}//inEnv
-	
-	def interpret(s: String) {
-		val ast = CmdParser.parse(s)
-		inEnv(Env()) exec ast
-	}//interpret
+  }
+
+  case class inEnv(env: Env) {
+    def eval(exp: Exp): OptVal = {
+      exp match {
+        case IntConst(v) => Some(IntValue(v))
+        case BoolConst(v) => Some(BoolValue(v))
+        case Var(name) =>
+          env(name) flatMap {
+            case Right(a) => Storage(a) // VAR
+            case Left(v) => Some(v) // CONST
+          }
+        case Greater(e1, e2) => eval2(e1, e2, { _ > _ })
+        case Less(e1, e2) => eval2(e1, e2, { _ < _ })
+        case Equal(e1, e2) => eval2(e1, e2, { _ == _ })
+        case Plus(e1, e2) => eval2(e1, e2, { _ + _ })
+        case Minus(e1, e2) => eval2(e1, e2, { _ - _ })
+        case Times(e1, e2) => eval2(e1, e2, { _ * _ })
+        case Div(e1, e2) => eval2(e1, e2, { _ / _ })
+      } //match
+    } // eval
+
+    private def eval2(e1: Exp, e2: Exp, op: (Value, Value) => OptVal) = {
+      for (
+        v1 <- eval(e1);
+        v2 <- eval(e2);
+        v3 <- op(v1, v2)
+      ) yield v3
+    }
+
+    def exec(cmd: Cmd) {
+      cmd match {
+        case If(e1, cThen, cElse) => {
+          eval(e1) map { b =>
+            if (b.isInstanceOf[BoolValue]) {
+              if (b.asInstanceOf[BoolValue].b) exec(cThen) else exec(cElse)
+            } // else ERROR
+          }
+        } //If
+
+        case Assign(Var(x), e) => {
+          val r = eval(e)
+          env(x) map {
+            case Right(a) => Storage.assign(a, r.get) //x denotes an address
+            case Left(a) => None // x denotes non-assignable value
+          }
+        } //Assign
+
+        case While(e: Exp, body: Cmd) =>
+          while (true) {
+            eval(e) map { b =>
+              if (b.isInstanceOf[BoolValue] &&
+                b.asInstanceOf[BoolValue].b) {
+                exec(body)
+              } else {
+                return
+              }
+            }
+          } // While
+
+        case Print(e) => {
+          println(">> " + { eval(e) getOrElse { e + " ist undefiniert" } })
+        }
+
+        case Block(defs, cmds) => {
+          val nEnv = define(defs)
+          cmds foreach { inEnv(nEnv) exec _ }
+        }
+
+        case Call(p, a) => {
+          eval(a) map { arg => env(p) map { 
+              case Right(adr) => None //x denotes an address Stored procedures may not be called
+              case Left(v) => // x denotes non-assignable value
+                v match {
+                  case procedure: ProcValue => procedure.appy(arg, env)
+                  case _ => println("ERROR: no procedure");
+                }
+            }
+          }
+
+        }
+
+      } // match
+    } // exec
+
+    def define(ds: List[Def]): Env = {
+      var nEnv: Env = env
+      ds foreach {
+        _ match {
+          case VarDef(x, initVal) => {
+            val v = eval(initVal)
+            val adr = Storage.allocateCell(v.get)
+            nEnv = nEnv + (x, adr)
+          }
+          case ConstDef(c, initVal) => {
+            val v = eval(initVal)
+            nEnv = nEnv + (c, v.get)
+          }
+          case ProcDef(p, fp, body) => {
+            nEnv = nEnv + (p, ProcValue(fp, body, nEnv))
+          }
+        }
+      }
+      nEnv
+    } // define
+
+  } //inEnv
+
+  def interpret(s: String) {
+    val ast = CmdParser.parse(s)
+    inEnv { Env() } exec ast
+  } //interpret
 
 }
 
--- src/main/scala/de/thm/pkr/Value.scala
+++ src/main/scala/de/thm/pkr/Value.scala
@@ -2,50 +2,50 @@ package de.thm.pkr
 
 // values of object language
 sealed class Value {
-  def + (v:Value): Option[Value] = None;
-  def - (v:Value): Option[Value] = None;
-  def * (v:Value): Option[Value] = None;
-  def / (v:Value): Option[Value] = None;
-  def < (v:Value): Option[Value] = None;
-  def > (v:Value): Option[Value] = None;
-  def == (v:Value): Option[Value] = None;
-  def appy (v:Value, callEnv: Interpreter.Env): Unit = None
+  type OptVal = Option[Value]
+  def + (v:Value): OptVal = None;
+  def - (v:Value): OptVal = None;
+  def * (v:Value): OptVal = None;
+  def / (v:Value): OptVal = None;
+  def < (v:Value): OptVal = None;
+  def > (v:Value): OptVal = None;
+  def == (v:Value): OptVal = None;
+  def appy (v:Value, callEnv: Interpreter.Env): Unit = ()
 }
 
 case class IntValue(n:Int) extends Value {
-
-  override def > (v: Value): Option[Value] = v match {
+  override def > (v: Value) = v match {
     case IntValue(nn) ⇒ Some(BoolValue(n == nn))
     case _ ⇒ None
   }
-  override def < (v: Value): Option[Value] = v match {
+  override def < (v: Value): OptVal = v match {
     case IntValue(nn) ⇒ Some(BoolValue(n < nn))
     case _ ⇒ None
   } 
-  override def == (v: Value): Option[Value] = v match {
+  override def == (v: Value): OptVal = v match {
     case IntValue(nn) ⇒ Some(BoolValue(n == nn))
     case _ ⇒ None
   } 
-  override def + (v: Value): Option[Value] = v match {
+  override def + (v: Value): OptVal = v match {
     case IntValue(nn) ⇒ Some(IntValue(n + nn))
     case _ ⇒ None
   }
-  override def - (v: Value): Option[Value] = v match {
+  override def - (v: Value): OptVal = v match {
     case IntValue(nn) ⇒ Some(IntValue(n - nn))
     case _ ⇒ None
   }
-  override def * (v: Value): Option[Value] = v match {
+  override def * (v: Value): OptVal = v match {
     case IntValue(nn) ⇒ Some(IntValue(n * nn))
     case _ ⇒ None
   }
-  override def / (v: Value): Option[Value] = v match {
+  override def / (v: Value): OptVal = v match {
     case IntValue(nn) ⇒ Some(IntValue(n / nn))
     case _ ⇒ None
   }
 }
 
 case class BoolValue(b: Boolean) extends Value {
-  override def == (v:Value): Option[Value] = v match {
+  override def == (v:Value): OptVal = v match {
     case BoolValue(bb) ⇒ Some(BoolValue(b == bb))
     case _ ⇒ None
   }
