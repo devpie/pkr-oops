fdce6c0f295c92f88ba57f80df1072a6dfda42c9
Jan Marco Müller <jan.m.mueller@mni.thm.de>
2013-01-25 18:06:12 +0100

    reverted the Value classes to the old design by Prof. Dr. Letschert

--- src/main/scala/de/thm/pkr/Value.scala
+++ src/main/scala/de/thm/pkr/Value.scala
@@ -3,56 +3,57 @@ package de.thm.pkr
 // values of object language
 sealed class Value {
   type OptVal = Option[Value]
-  def +(v: Value): OptVal = None;
-  def -(v: Value): OptVal = None;
-  def *(v: Value): OptVal = None;
-  def /(v: Value): OptVal = None;
-  def <(v: Value): OptVal = None;
-  def >(v: Value): OptVal = None;
-  def ==(v: Value): OptVal = None;
-  def appy(v: Value, callEnv: Interpreter.Env): Unit = ()
+  def + (v:Value): OptVal = None;
+  def - (v:Value): OptVal = None;
+  def * (v:Value): OptVal = None;
+  def / (v:Value): OptVal = None;
+  def < (v:Value): OptVal = None;
+  def > (v:Value): OptVal = None;
+  def == (v:Value): OptVal = None;
+  def appy (v:Value, callEnv: Interpreter.Env): Unit = ()
 }
 
-case class IntValue(n: Int) extends Value {
-
-  def to[B] = (op: (Int) => B) => PartialFunction[Value, B] { case IntValue(nn) => op(nn) }
-  //Some(v).map(to(nn => BoolValue(n > nn)))
-  override def >(v: Value) = Some(v).map(to(nn => BoolValue(n > nn)))
-
-  override def <(v: Value): OptVal = Some(v).map(to(nn => BoolValue(n < nn)))
-
-  override def ==(v: Value): OptVal = v match {
+case class IntValue(n:Int) extends Value {
+  override def > (v: Value) = v match {
     case IntValue(nn) ⇒ Some(BoolValue(n == nn))
     case _ ⇒ None
   }
-  override def +(v: Value): OptVal = v match {
+  override def < (v: Value): OptVal = v match {
+    case IntValue(nn) ⇒ Some(BoolValue(n < nn))
+    case _ ⇒ None
+  } 
+  override def == (v: Value): OptVal = v match {
+    case IntValue(nn) ⇒ Some(BoolValue(n == nn))
+    case _ ⇒ None
+  } 
+  override def + (v: Value): OptVal = v match {
     case IntValue(nn) ⇒ Some(IntValue(n + nn))
     case _ ⇒ None
   }
-  override def -(v: Value): OptVal = v match {
+  override def - (v: Value): OptVal = v match {
     case IntValue(nn) ⇒ Some(IntValue(n - nn))
     case _ ⇒ None
   }
-  override def *(v: Value): OptVal = v match {
+  override def * (v: Value): OptVal = v match {
     case IntValue(nn) ⇒ Some(IntValue(n * nn))
     case _ ⇒ None
   }
-  override def /(v: Value): OptVal = v match {
+  override def / (v: Value): OptVal = v match {
     case IntValue(nn) ⇒ Some(IntValue(n / nn))
     case _ ⇒ None
   }
 }
 
 case class BoolValue(b: Boolean) extends Value {
-  override def ==(v: Value): OptVal = v match {
+  override def == (v:Value): OptVal = v match {
     case BoolValue(bb) ⇒ Some(BoolValue(b == bb))
     case _ ⇒ None
   }
 }
 
 case class ProcValue(fp: String, body: Cmd, defEnv: Interpreter.Env) extends Value {
-  override def appy(v: Value, callEnv: Interpreter.Env): Unit = {
-    val exec_env: Interpreter.Env = defEnv + (fp, v)
+  override def appy (v:Value, callEnv: Interpreter.Env): Unit = {
+    val exec_env: Interpreter.Env = defEnv+(fp, v)
     Interpreter.inEnv(exec_env) exec body
   }
 }
